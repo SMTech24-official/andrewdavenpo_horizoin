c754e1bfa5b174a78fe556af005b55d6b07f5381
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-01-29 16:11:32 +0600

    refactor: simplify Shop component by removing unused code and integrating OurShopSlider

--- src/components/home/Shop.tsx
+++ src/components/home/Shop.tsx
@@ -1,57 +1,8 @@
 "use client";
 
-import Image from "next/image";
-import React, { useState } from "react";
-import { IoIosArrowForward } from "react-icons/io";
-
-import books from "@/assets/home/book.png";
-import { IoIosArrowBack } from "react-icons/io";
-import { HiOutlineShoppingCart } from "react-icons/hi";
-import Link from "next/link";
 import OurShopSlider from "./OurShopSlider";
 
-const courses = [
-  {
-    id: 1,
-    title: "English Textbook for Class 12",
-    discountPrice: "170",
-    OldPrice: "200",
-    image: books,
-  },
-  {
-    id: 2,
-    title: "English Textbook for Class 12",
-    discountPrice: "170",
-    OldPrice: "200",
-    image: books,
-  },
-  {
-    id: 3,
-    title: "English Textbook for Class 12",
-    discountPrice: "170",
-    OldPrice: "200",
-    image: books,
-  },
-  {
-    id: 4,
-    title: "English Textbook for Class 12",
-    discountPrice: "170",
-    OldPrice: "200",
-    image: books,
-  },
-];
-
 export default function Shop() {
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  const nextSlide = () => {
-    setCurrentSlide((prev) => (prev === courses.length - 3 ? 0 : prev + 1));
-  };
-
-  const prevSlide = () => {
-    setCurrentSlide((prev) => (prev === 0 ? courses.length - 3 : prev - 1));
-  };
-
   return (
     <div className=" bg-white mx-auto px-4 py-[80px] sm:px-6 lg:px-8">
       <div className="container mx-auto">
