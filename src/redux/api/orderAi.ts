94c6fbc35d229e6c2427c86ab01c6b0b5424510c
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-01-30 18:42:27 +0600

    feat: add order API endpoints for fetching and creating orders; update base API tag types

---
+++ src/redux/api/orderAi.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { baseApi } from "./baseApi";
+
+const orderApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    // get all orders
+    getAllOrders: build.query({
+      query: () => ({
+        url: `/orders`,
+        method: "GET",
+      }),
+      providesTags: ["Orders"],
+    }),
+
+    createOrder: build.mutation({
+      query: (data: any) => {
+        return {
+          url: `/create-order`,
+          method: "POST",
+          body: data,
+        };
+      },
+      invalidatesTags: ["Orders"],
+    }),
+  }),
+});
+
+export const { useGetAllOrdersQuery, useCreateOrderMutation } = orderApi;
