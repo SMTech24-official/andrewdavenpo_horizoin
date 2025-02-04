de94cfa79fbb538add45dfb56d98e18d9cddd9aa
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-02-04 13:20:42 +0600

    feat: refactor EducationCardSlider to use grid layout and improve EducationalResourcesCard styling

--- src/app/(withLayout)/page.tsx
+++ src/app/(withLayout)/page.tsx
@@ -2,7 +2,7 @@ import Education from "@/components/home/Education";
 import Herosection from "@/components/home/Herosection";
 import Shop from "@/components/home/Shop";
 import Subscribe from "@/components/home/Subscribe";
-import WhoAreYou from "@/components/home/WhoAreYou";
+// import WhoAreYou from "@/components/home/WhoAreYou";
 import homeHeroImage from "@/assets/image 38.png";
 
 export default function Home() {
@@ -14,7 +14,7 @@ export default function Home() {
         actions={true}
         heroImg={homeHeroImage.src}
       />
-      <WhoAreYou />
+      {/* <WhoAreYou /> */}
       <Education />
       <Shop />
       <Subscribe />
--- src/components/home/EducationCardSlider.tsx
+++ src/components/home/EducationCardSlider.tsx
@@ -1,14 +1,12 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { Navigation, Pagination } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { SwiperSlide } from "swiper/react";
 
-import EducationalResourcesCard from "../shared/EducationalResourcesCard";
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import { useGetAllVideoQuery } from "@/redux/api/videoApi";
+import EducationalResourcesCard from "../shared/EducationalResourcesCard";
 interface Video {
   id: string;
   title: string;
@@ -19,8 +17,6 @@ interface Video {
 }
 
 function EducationCardSlider() {
-  const prevRef = useRef<HTMLButtonElement | null>(null);
-  const nextRef = useRef<HTMLButtonElement | null>(null);
   const [mounted, setMounted] = useState(false);
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { data, isLoading } = useGetAllVideoQuery(undefined);
@@ -35,71 +31,14 @@ function EducationCardSlider() {
   }
 
   return (
-    <div className="relative my-20">
-      <div className="mb-6 absolute -top-20 right-0">
-        <div className="flex gap-2">
-          <button
-            ref={prevRef}
-            className="  bg-[#404347] rounded-md text-white border p-2  shadow-md hover:bg-primary hover:text-white transition"
-          >
-            <FaArrowLeft />
-          </button>
-          <button
-            ref={nextRef}
-            className=" bg-[#404347] rounded-md text-white border p-2  shadow-md hover:bg-primary hover:text-white transition"
-          >
-            <FaArrowRight />
-          </button>
-        </div>
-      </div>
-      <Swiper
-        modules={[Navigation, Pagination]}
-        loop={true}
-        navigation={{
-          prevEl: prevRef.current,
-          nextEl: nextRef.current,
-        }}
-        onBeforeInit={(swiper) => {
-          if (swiper.params.navigation && typeof swiper.params.navigation !== "boolean") {
-            swiper.params.navigation.prevEl = prevRef.current;
-            swiper.params.navigation.nextEl = nextRef.current;
-          }
-        }}
-        centeredSlides={false}
-        breakpoints={{
-          540: {
-            slidesPerView: 1,
-            spaceBetween: 10,
-            centeredSlides: true,
-          },
-          768: {
-            slidesPerView: 2,
-            spaceBetween: 10,
-            centeredSlides: false,
-          },
-          1024: {
-            slidesPerView: 3,
-            spaceBetween: 14,
-            centeredSlides: false,
-          },
-          1280: {
-            slidesPerView: 3,
-            spaceBetween: 14,
-          },
-        }}
-        className="mySwiper"
-      >
-        {videos?.map((item: Video) => (
-          <SwiperSlide key={item.id}>
-            <EducationalResourcesCard item={item} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+    <div className="grid grid-cols-3 gap-10 my-20">
+      {videos?.slice(0, 3)?.map((item: Video) => (
+        <SwiperSlide key={item.id}>
+          <EducationalResourcesCard item={item} />
+        </SwiperSlide>
+      ))}
     </div>
   );
 }
 
 export default EducationCardSlider;
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const cateGoryCards = [0, 1, 2, 3, 4, 5, 6];
