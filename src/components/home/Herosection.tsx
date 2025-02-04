ae2b8b81458c897c2bee0733a7dd401e17bd2ad1
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-02-04 11:41:38 +0600

    feat: add overlay prop to Herosection component for enhanced background styling

--- src/components/home/Herosection.tsx
+++ src/components/home/Herosection.tsx
@@ -1,73 +1,54 @@
 "use client";
 import Image from "next/image";
-// import heroImg from "@/assets/home/hero_girl.png";
-// import barnnerImage from "@/assets/Hero-section.png";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 interface HeroSectionProps {
   heroImg: string;
   actions?: boolean;
   subHeading?: string;
   heading: string;
+  overlay?: boolean;
 }
 
-export default function Herosection({ heroImg, heading, subHeading, actions }: HeroSectionProps) {
-  const pathname = usePathname();
-
+export default function Herosection({ heroImg, heading, subHeading, actions, overlay }: HeroSectionProps) {
   return (
-    <div
-      className="pt-28 pb-20  bg-black"
-      // // background image
-      // style={{
-      //   backgroundImage: `url(${barnnerImage.src})`,
-      //   backgroundSize: "cover",
-      //   backgroundPosition: "center",
-      //   backgroundRepeat: "no-repeat",
-      // }}
-    >
-      <div className="container mx-auto">
+    <div className="pt-28 pb-20  bg-black">
+      <div className="container relative mx-auto">
+        {overlay && (
+          <div // give an overlay to the image from left to right (black to transparent)
+            style={{
+              position: "absolute",
+              zIndex: 1,
+              width: "100%",
+              height: "100%",
+              background: "linear-gradient(90deg, rgba(0, 0, 0, 0.8) 70%, rgba(0, 0, 0, 0) 90%)",
+            }}
+          ></div>
+        )}
         <div className="grid lg:grid-cols-2 items-center justify-between">
-          {pathname === "/" ? (
-            <div>
-              <h2 className="font-bold text-5xl text-white leading-[62px] pb-4">{heading}</h2>
-              {subHeading && <p className="text-white text-[20px]">{subHeading}</p>}
-              {actions && (
-                <div className="flex gap-6 pt-10">
-                  <Link
-                    href={"/"}
-                    className="py-[10px] border-2 px-[20px] rounded-[8px] bg-white text-[18px] text-black  transition-all"
-                  >
-                    Explore more
-                  </Link>
-                  <Link
-                    href={"/"}
-                    className="py-[10px] px-[20px] rounded-[8px]  text-[18px] border-2  border-white bg-transparent text-white transition-all"
-                  >
-                    Shop Now
-                  </Link>
-                </div>
-              )}
-            </div>
-          ) : pathname === "/education" ? (
-            <div>
-              <h2 className="font-bold text-5xl text-white leading-[62px] pb-4">Educational Resources </h2>
-              <p className="text-white text-[20px]">Discover new skills and exclusive products in one place.</p>
-            </div>
-          ) : pathname === "/store" ? (
-            <div>
-              <h2 className="font-bold text-5xl text-white leading-[62px] pb-4">Our shop</h2>
-              <p className="text-white text-[20px]">Find Everything You Need in One Place</p>
-            </div>
-          ) : (
-            <div>
-              <h2 className="font-bold text-5xl text-white leading-[62px] pb-4">Members Area</h2>
-              <p className="text-white text-[20px]">Special Content, Special Benefits—Just for Members</p>
-            </div>
-          )}
+          <div className="" style={{ zIndex: 2, position: "relative" }}>
+            <h2 className="font-bold text-5xl text-white leading-[62px] pb-4">{heading}</h2>
+            {subHeading && <p className="text-white text-[20px]">{subHeading}</p>}
+            {actions && (
+              <div className="flex gap-6 pt-10">
+                <Link
+                  href={"/"}
+                  className="py-[10px] border-2 px-[20px] rounded-[8px] bg-white text-[18px] text-black  transition-all"
+                >
+                  Explore more
+                </Link>
+                <Link
+                  href={"/"}
+                  className="py-[10px] px-[20px] rounded-[8px]  text-[18px] border-2  border-white bg-transparent text-white transition-all"
+                >
+                  Shop Now
+                </Link>
+              </div>
+            )}
+          </div>
 
-          <div className="flex justify-end">
-            <Image src={heroImg} width={392} height={648} alt="heroImg" />
+          <div className="flex  justify-end">
+            <Image src={heroImg} width={392} height={648} alt="heroImg" className="h-full w-full rounded-lg" />
           </div>
         </div>
       </div>
