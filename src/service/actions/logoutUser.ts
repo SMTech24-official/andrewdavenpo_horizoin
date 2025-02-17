123986c250ab2e459b82b646855a4f975beabe45
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-02-17 11:20:50 +0600

    feat: update logoutUser action to remove accessToken and clean up commented code

--- src/service/actions/logoutUser.ts
+++ src/service/actions/logoutUser.ts
@@ -2,22 +2,23 @@ import { authKey } from "@/constants/authkey";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 import { deleteCookies } from "./deleteCookies";
 
-const backendUrl = process.env.NEXT_PUBLIC_BASEURL;
+// const backendUrl = process.env.NEXT_PUBLIC_BASEURL;
 
 export const logoutUser = async (router: AppRouterInstance) => {
-  const response = await fetch(`${backendUrl}/auth/logout`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `${localStorage.getItem(authKey)}`,
-    },
-    credentials: "include",
-  });
+  // const response = await fetch(`${backendUrl}/auth/logout`, {
+  //   method: "PATCH",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     Authorization: `${localStorage.getItem(authKey)}`,
+  //   },
+  //   credentials: "include",
+  // });
 
-  console.log(response);
+  // console.log(response);
 
   localStorage.removeItem(authKey);
   deleteCookies([authKey, "token"]);
+  deleteCookies([authKey, "accessToken"]);
 
   router.push("/");
   router.refresh();
