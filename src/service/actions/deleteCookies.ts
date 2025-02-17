ee09b2c8386afcf36d7e6a9a07440abb721cc774
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-02-17 11:20:45 +0600

    feat: refactor deleteCookies function for improved async handling

--- src/service/actions/deleteCookies.ts
+++ src/service/actions/deleteCookies.ts
@@ -1,10 +1,20 @@
+// "use server";
+
+// import { cookies } from "next/headers";
+
+// export const deleteCookies = async (keys: string[]) => {
+//   keys.forEach(async (key) => {
+//     const cookieStore = await cookies();
+//     cookieStore.delete(key);
+//   });
+// };
 "use server";
 
 import { cookies } from "next/headers";
 
-export const deleteCookies = (keys: string[]) => {
-  keys.forEach(async (key) => {
-    const cookieStore = await cookies();
+export const deleteCookies = async (keys: string[]) => {
+  const cookieStore = await cookies();
+  keys.forEach((key) => {
     cookieStore.delete(key);
   });
 };
